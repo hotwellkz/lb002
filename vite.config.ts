4cdd1102bd53e2fddd14a6d106444b7bfd013232
hotwellkz <hotwell.kz@gmail.com>
2024-12-30 16:57:59 +0600

    Initial commit

---
+++ vite.config.ts
@@ -0,0 +1,73 @@
+import UnoCSS from 'unocss/vite';
+import { defineConfig, type ViteDevServer } from 'vite';
+import { nodePolyfills } from 'vite-plugin-node-polyfills';
+import { optimizeCssModules } from 'vite-plugin-optimize-css-modules';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import { vitePlugin as remixVitePlugin } from '@remix-run/dev';
+
+export default defineConfig((config) => {
+  return {
+    base: './', // Для корректного отображения относительных путей на Netlify
+    build: {
+      target: 'esnext', // Современные браузеры
+      outDir: 'build/client', // Папка для готовых файлов
+      emptyOutDir: true, // Очищает папку перед сборкой
+    },
+    plugins: [
+      nodePolyfills({
+        include: ['path', 'buffer'], // Полифиллы для Node.js
+      }),
+      remixVitePlugin({
+        future: {
+          v3_fetcherPersist: true,
+          v3_relativeSplatPath: true,
+          v3_throwAbortReason: true,
+        },
+      }),
+      UnoCSS(), // Поддержка UnoCSS
+      tsconfigPaths(), // Поддержка путей из tsconfig
+      chrome129IssuePlugin(), // Плагин для локальной разработки
+      config.mode === 'production' && optimizeCssModules({ apply: 'build' }),
+    ],
+    envPrefix: [
+      'VITE_',
+      'OPENAI_LIKE_API_',
+      'OLLAMA_API_BASE_URL',
+      'LMSTUDIO_API_BASE_URL',
+      'TOGETHER_API_BASE_URL',
+    ],
+    css: {
+      preprocessorOptions: {
+        scss: {
+          api: 'modern-compiler',
+        },
+      },
+    },
+  };
+});
+
+function chrome129IssuePlugin() {
+  return {
+    name: 'chrome129IssuePlugin',
+    configureServer(server: ViteDevServer) {
+      server.middlewares.use((req, res, next) => {
+        const raw = req.headers['user-agent']?.match(/Chrom(e|ium)\/([0-9]+)\./);
+
+        if (raw) {
+          const version = parseInt(raw[2], 10);
+
+          if (version === 129) {
+            res.setHeader('content-type', 'text/html');
+            res.end(
+              '<body><h1>Please use Chrome Canary for testing.</h1><p>Chrome 129 has an issue with JavaScript modules & Vite local development, see <a href="https://github.com/stackblitz/bolt.new/issues/86#issuecomment-2395519258">for more information.</a></p><p><b>Note:</b> This only impacts <u>local development</u>. `pnpm run build` and `pnpm run start` will work fine in this browser.</p></body>',
+            );
+
+            return;
+          }
+        }
+
+        next();
+      });
+    },
+  };
+}
